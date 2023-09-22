d77500afdcdcea0a30d9254c3713099531acbcd2
RoAd-ZU <ZUZERN.AD@gmail.com>
2023-09-23 00:39:31 +0300

    main.py - ready csv_reader.py - ready

--- homework-1/main.py
+++ homework-1/main.py
@@ -1,16 +1,21 @@
+"""Скрипт для заполнения данными таблиц в БД Postgres."""
 import psycopg2
 import os.path
-from north_data.csv_reader import create_employees, create_customers, create_orders
-
-"""Скрипт для заполнения данными таблиц в БД Postgres."""
+from north_data.csv_reader import create_data
 
 
 def create_table():
     password = input('Введите свой пароль postgres\n')
     table1 = psycopg2.connect(host='localhost', database='north', user='postgres', password=password)
-    data1 = create_employees()
-    data2 = create_customers()
-    data3 = create_orders()
+
+    file1 = os.path.realpath('north_data/employees_data.csv')
+    file2 = os.path.realpath('north_data/customers_data.csv')
+    file3 = os.path.realpath('north_data/orders_data.csv')
+
+    data1 = create_data(file1)
+    data2 = create_data(file2)
+    data3 = create_data(file3)
+
     try:
         with table1:
             for i in data1:
@@ -18,19 +23,12 @@ def create_table():
                     with table1.cursor() as curso:
                         curso.execute("INSERT INTO employees VALUES (%s, %s, %s, %s, %s, %s)", (i))
 
-    finally:
-        table1.close()
-
-    try:
         with table1:
             for i in data2:
                 if i != data2[0]:
                     with table1.cursor() as curso:
                         curso.execute("INSERT INTO customers VALUES (%s, %s, %s)", (i))
-    finally:
-        table1.close()
 
-    try:
         with table1:
             for i in data3:
                 if i != data3[0]:
--- homework-1/north_data/csv_reader.py
+++ homework-1/north_data/csv_reader.py
@@ -1,34 +1,11 @@
+"""Файл для получения данных из файлов в north_data"""
 import csv
 
-def create_employees():
+
+def create_data(file):
     data = []
-    with open('employees_data.csv', 'r', newline='') as csvfile:
+    with open(file, 'r', newline='') as csvfile:
         filedata = csv.reader(csvfile)
         for i in filedata:
             data.append(i)
     return data
-
-
-def create_customers():
-    data = []
-    with open('customers_data.csv', 'r', newline='') as csvfile:
-        filedata = csv.reader(csvfile)
-        for i in filedata:
-            data.append(i)
-    return data
-
-
-def create_orders():
-    data_employees = create_employees()
-    data_customers = create_customers()
-    data = []
-    with open('orders_data.csv', 'r', newline='') as csvfile:
-        filedata = csv.reader(csvfile)
-        for i in filedata:
-            data.append(i)
-    return data
-
-# ty1 = create_employees()
-# ty2 = create_customers()
-# ty3 = create_orders()
-# print(ty3)
